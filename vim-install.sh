30706078e9ccb6c20a86261c3ff4c1dc84cfc8f1
Greg Allen <me@jga.me>
2012-12-12 21:10:43 -0800

    new vim-install script

---
+++ vim-install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+vim -u ~/.vim/bundles.vim +BundleInstall +q
--- vim-remotes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for i in ~/dotfiles/.vim/bundle/*; do
-	if [ -d "$i/.git" ] || [ -f "$i/.git" ]; then
-		echo $i
-		cd $i
-    git remote -v
-	fi
-done
